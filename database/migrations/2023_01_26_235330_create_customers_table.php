e8c9baa5aa69a88e6ab8b0e6c73cbb2292a51314
Felipe Perucci Machado do Amaral <felipe.machado.amaral@gmail.com>
2023-01-27 00:09:15 -0300

    customers migration improvement

--- database/migrations/2023_01_26_235330_create_customers_table.php
+++ database/migrations/2023_01_26_235330_create_customers_table.php
@@ -14,14 +14,14 @@ return new class() extends Migration {
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->string('phone');
-            $table->date('bornDate');
-            $table->string('adress');
-            $table->string('adressComplement');
-            $table->string('district');
-            $table->string('cep');
+            $table->string('name')->nullable();
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->date('bornDate')->nullable();
+            $table->string('adress')->nullable();
+            $table->string('adressComplement')->nullable();
+            $table->string('district')->nullable();
+            $table->string('cep')->nullable();
             $table->timestamps();
         });
     }
