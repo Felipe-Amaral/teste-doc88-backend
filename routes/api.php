a990d7639ac1659889aa5d69d172c3648f429e67
Felipe Perucci Machado do Amaral <felipe.machado.amaral@gmail.com>
2023-01-27 00:10:14 -0300

    api route remove unused methods

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\CustomerController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::resource('customer', CustomerController::class);
+Route::resource('customer', CustomerController::class,
+    [
+        'except' => ['create', 'edit'],
+    ]
+);
