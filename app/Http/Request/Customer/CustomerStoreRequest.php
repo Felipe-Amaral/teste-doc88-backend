44b22c556524f48b0070c1a97bcec9da5b813317
Felipe Perucci Machado do Amaral <felipe.machado.amaral@gmail.com>
2023-01-27 22:36:30 -0300

    customer requests

---
+++ app/Http/Request/Customer/CustomerStoreRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Customer;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CustomerStoreRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $rules = [
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'phone' => 'required|string',
+            'bornDate' => 'required|string',
+            'adress' => 'required|string',
+            'adressComplement' => 'required|string',
+            'district' => 'required|string',
+            'cep' => 'required|string',
+        ];
+
+        return $rules;
+    }
+}
